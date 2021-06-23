a0abcca6325d8e66d5ae805de8ae24f4984e9f61
nasanov <asanoff.n@gmail.com>
2021-06-23 01:36:12 -0500

    first version of darkmode

--- assets/js/darkmode.js
+++ assets/js/darkmode.js
@@ -1,12 +1,23 @@
 window.addEventListener('DOMContentLoaded', () => {
 	document.getElementById('darkmode-switch').addEventListener('click', () => {
-		console.log('hello world');
 		if (window.getComputedStyle(document.documentElement).getPropertyValue('--background-color') === '#fff') {
 			document.documentElement.style.setProperty('--background-color', '#333');
+			document.documentElement.style.setProperty('--background-color-secondary', '#1f2223');
 			document.documentElement.style.setProperty('--text-color', '#fff');
+			document.documentElement.style.setProperty('--skills-background', '#1f2223');
+			document.documentElement.style.setProperty('--section-title', '#fdfdfd');
+			document.documentElement.style.setProperty('--about-text-color', '#fdfdfd');
+			document.documentElement.style.setProperty('--contact-form-shadow', '#1f2223');
+			document.documentElement.style.setProperty('--contact-form-background', '#1f2223');
 		} else {
 			document.documentElement.style.setProperty('--background-color', '#fff');
+			document.documentElement.style.setProperty('--background-color-secondary', '#f2f2f2');
 			document.documentElement.style.setProperty('--text-color', '#333');
+			document.documentElement.style.setProperty('--skills-background', '#f0f0f0');
+			document.documentElement.style.setProperty('--section-title', '#252525');
+			document.documentElement.style.setProperty('--about-text-color', '#040404');
+			document.documentElement.style.setProperty('--contact-form-shadow', '#ededed');
+			document.documentElement.style.setProperty('--contact-form-background', '#fff');
 		}
 	});
 });
